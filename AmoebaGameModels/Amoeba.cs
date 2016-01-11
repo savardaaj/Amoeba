6aaec234931affbb6500f125b029d1cda8279e57
rpkelly <rpkelly@g.clemson.edu>
2016-01-11 15:24:10 -0500

    added AmoebaServer and AmoebaGameModels projects

---
+++ AmoebaGameModels/Amoeba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AmoebaServer
+{
+    public enum Direction
+    {
+        Still     = 0,
+        North     = 1,
+        South     = 2,
+        East      = 3,
+        West      = 4,
+        Northeast = 5,
+        Northwest = 6,
+        Southeast = 7,
+        Southwest = 8,
+    }
+    class Amoeba
+    {
+        
+        public Direction Direction { get; set; }
+        private Int32 Size { get; set; }
+        private Int32 Speed { get; set; }
+        private Int32 XCoordinate { get; set; }
+        private Int32 YCoordinate { get; set; }
+    }
+}
