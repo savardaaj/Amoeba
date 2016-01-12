6a9858768e096eeb40e5264aca9a0b49688cbc28
rpkelly <rpkelly@g.clemson.edu>
2016-01-12 14:01:22 -0500

    Ameoba and Gameboard classes added to dll

--- AmoebaGameModels/Amoeba.cs
+++ AmoebaGameModels/Amoeba.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AmoebaServer
+namespace AmoebaGameModels
 {
     public enum Direction
     {
@@ -17,13 +17,46 @@ namespace AmoebaServer
         Southeast = 7,
         Southwest = 8,
     }
+
     class Amoeba
     {
-        
+        #region Public Fields
         public Direction Direction { get; set; }
-        private Int32 Size { get; set; }
-        private Int32 Speed { get; set; }
-        private Int32 XCoordinate { get; set; }
-        private Int32 YCoordinate { get; set; }
+        #endregion
+
+        #region Public Properties
+        //All players will begin at size 1.  Default size for food is .25
+        public Decimal Size { get { return this.size; } }
+        public Decimal Speed { get; }
+        public Decimal XCoordinate { get; }
+        public Decimal YCoordinate { get; }
+        #endregion
+
+        #region Private Fields
+        private Decimal size;
+        private Decimal speed;
+        private Decimal xcoordinate;
+        private Decimal ycoordinate;
+        #endregion
+
+        #region Public Constructors
+        public Amoeba ()
+        {
+            
+        }
+        #endregion
+
+        #region Public Methods
+
+        public Decimal Eat (Amoeba Food)
+        {
+            this.size += Food.Size;
+            this.speed = 22 * Math.Pow(this.size, -0.439);
+            return this.size;
+        }
+
+
+
+        #endregion
     }
 }
---
+++ AmoebaGameModels/GameBoard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AmoebaGameModels
+{
+    class GameBoard
+    {
+        Int32 Width;
+        Int32 Height;
+
+    }
+}
