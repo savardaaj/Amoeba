14e7c45bd96cbabf17b95d324ac36ac7ebe4c883
kevinvs757 <kevinvs757@gmail.com>
2016-01-15 09:31:40 -0500

    fixed some merge problems

--- AmoebaGameModels/Amoeba.cs
+++ AmoebaGameModels/Amoeba.cs
@@ -12,7 +12,7 @@ namespace AmoebaGameModels
         //The slope in the equation y=mx+b
         public Decimal Slope    { get; set; }
         public Guid    CellId   { get; set; }
-        public Boolean Wordy    { get; set; }
+        public Boolean Wordy = false;
         
         public Decimal XCoordinate { get; set; }
         public Decimal YCoordinate { get; set; }
@@ -23,33 +23,23 @@ namespace AmoebaGameModels
         #region Public Properties
         //All players will begin at size 1.  Default size for food is .25
         public Decimal Size { get { return this.size; } }
-<<<<<<< HEAD
-        public Decimal Speed { get { return /*this.speed;*/ this.maxspeed; } }
-        public Decimal MaxSpeed { get { return this.maxspeed; } }
-        public Decimal MaxTravelDistance { get { return this.maxtraveldistance; } }
-
-=======
         public Decimal Speed { get { return this.speed; } }
-        public Decimal XCoordinate { get; set; }
-        public Decimal YCoordinate { get; set; }
+        public Decimal MaxTravelDistance { get { return this.maxtraveldistance; } }
         public Texture2D texture { get; set; }
->>>>>>> 9ff5f857fe03186f028e1baff407c919ce6a9ea0
         #endregion
 
         #region Private Fields
         private Decimal size;
         private Decimal speed;
-        private Decimal maxspeed;
         private Decimal maxtraveldistance;
         #endregion
 
         #region Public Constructors
         public Amoeba()
         {
-            this.Wordy = false;
             CellId = Guid.NewGuid ();
             this.size = (Decimal).25;
-            this.maxspeed = (Decimal).05;
+            this.speed = (Decimal).05;
             this.maxtraveldistance = (Decimal)2;
         }
 
@@ -66,8 +56,7 @@ namespace AmoebaGameModels
         public Decimal Eat(Amoeba Food)
         {
             this.size += Food.Size;
-            //this.speed = (Decimal)1.5 * Convert.ToDecimal(Math.Pow(Convert.ToDouble(this.size), -0.439));
-            //this.maxspeed = 2;
+            //this.maxtraveldistance = (Decimal)1.5 * Convert.ToDecimal(Math.Pow(Convert.ToDouble(this.size), -0.439));
             return this.size;
         }
 
