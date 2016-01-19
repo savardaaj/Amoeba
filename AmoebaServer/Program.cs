d148834a400729038e0f467ea4af5a93824abb24
rpkelly <rpkelly@g.clemson.edu>
2016-01-19 16:39:24 -0500

    Initial Networking code commit

--- AmoebaServer/Program.cs
+++ AmoebaServer/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AmoebaServer
 {
@@ -9,6 +11,23 @@ namespace AmoebaServer
     {
         static void Main (string [] args)
         {
+            Server serverObject = new Server ();
+
+            Task.Factory.StartNew (async () =>
+            {
+                
+                serverObject.Listen ();
+                
+            });
+            int packets = 0;
+            do
+            {
+                if(serverObject.DataList.Count > packets)
+                { 
+                    Console.WriteLine (serverObject.DataList[packets]);
+                    packets++;
+                }
+            } while (true);
         }
     }
 }
